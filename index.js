1adfd8c67fc9a591fa193b3d8003d907ade792ab
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-31 15:54:03 +0530

    COMPLETED INDEX.JS IN CLEAN BLOG-02 PROJECT

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const path = require('path')
 
 const app = express();
 
+/*WITH THIS CODE,EXPRESS WILL ACCESS ALL 
+THE FOLDERS IN STATIC ASSETS IN THE PUBLIC DIRECTORY */
+app.use(express.static('public'));
+
+app.listen(4000,()=>{
+    console.log('App is Running in 4000 port');
+    
+}
+)
