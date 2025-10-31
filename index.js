7dcf97110b5f1b081e49ca1210ede0d12c604dc5
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-31 15:56:54 +0530

    STARTED RECTIFYING LINKS IN CLEAN BLOG-02 PROJECT

--- index.js
+++ index.js
@@ -10,5 +10,20 @@ app.use(express.static('public'));
 app.listen(4000,()=>{
     console.log('App is Running in 4000 port');
     
-}
-)
+});
+
+
+app.get('/',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+});
+
+app.get('/about',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'pages/about.html'))
+});
+
+app.get('/content',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'pages/content.html'))
+});
+app.get('/post',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'pages/post.html'))
+});
